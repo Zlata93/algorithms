e532acf61197c95528bedcfd1ded3c3a9af87d69
Zlata Kotlova <zlata.kotlova@mail.ru>
2019-10-20 12:17:48 +0300

    selection sort

--- index.js
+++ index.js
@@ -13,4 +13,22 @@ function bubbleSort(arr) {
     }
     return arr;
 }
-console.log(bubbleSort(unsortedArr));
+// console.log(bubbleSort(unsortedArr));
+
+function selectionSort(arr) {
+    let indexOfMax = 0;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i; j++) {
+            if (arr[j] > arr[indexOfMax]) {
+                indexOfMax = j;
+            }
+        }
+        if (i !== indexOfMax) {
+            const temp = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = arr[indexOfMax];
+            arr[indexOfMax] = temp;
+        }
+    }
+    return arr;
+}
+console.log(selectionSort(unsortedArr));
