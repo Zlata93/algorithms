765a050763f7639bf839f826fd044f8aea713781
Zlata Kotlova <zlata.kotlova@mail.ru>
2019-10-20 11:50:17 +0300

    bubble sort attempt 2

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ console.log('Hi');
 
 const unsortedArr = [5,1,9,0,2,-1,7];
 function bubbleSort(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i+1; j < arr.length; j++) {
-            if (arr[j] < arr[i]) {
-                const smaller = arr[j];
-                arr[j] = arr[i];
-                arr[i] = smaller;
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - i; j++) {
+            if (arr[j] > arr[j+1]) {
+                const bigger = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = bigger;
             }
         }
     }
