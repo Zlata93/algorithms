ad74b49d215e5d07de9ff779d64caffae3864867
Zlata Kotlova <zlata.kotlova@mail.ru>
2019-10-20 16:04:41 +0300

    get primes

--- index.js
+++ index.js
@@ -125,5 +125,26 @@ function balanceBrackets(str) {
     }
     return !stack.length;
 }
-console.log(balanceBrackets(unbalancedStr));
-console.log(balanceBrackets(balancedStr));
+// console.log(balanceBrackets(unbalancedStr));
+// console.log(balanceBrackets(balancedStr));
+
+function isPrime(num) {
+    if (num < 2) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+function getPrimes(n) {
+    const primes = [];
+    for (let i = 2; i < n; i++) {
+        if (isPrime(i)) {
+            primes.push(i);
+        }
+    }
+    return primes;
+}
+console.log(getPrimes(1000));
+console.log(getPrimes(1000).length);
